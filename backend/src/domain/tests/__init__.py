248028c62151b4ec64285e8c929fd99b81ec7aba
teo-montero <teo.bonet.24@ucl.ac.uk>
2026-02-16 12:05:05 +0000

    task: updated package information to include test imports

---
+++ backend/src/domain/tests/__init__.py
@@ -0,0 +1,14 @@
+from .test import Test
+from .model_spec import ModelSpec, ModelType
+from .environment_spec import EnvironmentSpec, EnvType
+from .runner_spec import RunnerSpec, RunnerType
+
+__all__ = [
+    "Test",
+    "ModelSpec",
+    "ModelType",
+    "EnvironmentSpec",
+    "EnvType",
+    "RunnerSpec",
+    "RunnerType"
+]
