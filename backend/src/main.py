4a60d62d3826d85835576c89d96c4132c1d1a066
teo-montero <teo.bonet.24@ucl.ac.uk>
2026-02-25 17:58:14 +0000

    task: updated main to include container

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 from infra.persistance.db import Base, engine
 from api.routes import test_router
+from container import Container
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+container = Container()
+
+app.state.container = container
+
 app.include_router(test_router)
