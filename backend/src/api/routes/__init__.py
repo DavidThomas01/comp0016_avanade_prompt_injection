9b9374b389a012d79d9ba252bbd994e56375c8e2
teo-montero <teo.bonet.24@ucl.ac.uk>
2026-02-25 17:57:43 +0000

    task: added package description files

---
+++ backend/src/api/routes/__init__.py
@@ -0,0 +1,5 @@
+from .test_routes import router as test_router
+
+__all__ = [
+    "test_router"
+]
---
+++ backend/src/app/tests/dto/__init__.py
@@ -0,0 +1,10 @@
+from .test_input import ModelSpecInput, EnvironmentSpecInput, RunnerSpecInput, CreateTestInput, UpdateTestInput, RunTestInput
+
+__all__ = [
+    "ModelSpecInput",
+    "EnvironmentSpecInput",
+    "RunnerSpecInput",
+    "CreateTestInput",
+    "UpdateTestInput",
+    "RunTestInput"
+]
