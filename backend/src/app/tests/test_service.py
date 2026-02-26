7a0a2e6254d5fa2adcfd5e5c9811eadbd27868aa
teo-montero <teo.bonet.24@ucl.ac.uk>
2026-02-26 08:37:13 +0000

    task: implemented custom builder methods to enforce DDD

--- backend/src/app/tests/test_service.py
+++ backend/src/app/tests/test_service.py
@@ -21,9 +21,9 @@ class TestService():
         
     
     def create(self, db: Session, request: CreateTestInput) -> Test:
-        model_spec = ModelSpec.create_from_input(request.model)
-        environment_spec = EnvironmentSpec.create_from_input(request.environment)
-        runner_spec = RunnerSpec.create_from_input(request.runner)
+        model_spec = self._build_model(request.model)
+        environment_spec = self._build_environment(request.environment)
+        runner_spec = self._build_runner(request.runner)
         
         test = Test.create(
             name=request.name,
@@ -39,9 +39,9 @@ class TestService():
         parent = self.repo.get_by_id(db, parent_id)
         
         name = name if request.name else parent.name
-        model_spec = ModelSpec.create_from_input(request.model) if request.model else parent.model
-        environment_spec = EnvironmentSpec.create_from_input(request.environment) if request.environment else parent.environment
-        runner_spec = RunnerSpec.create_from_input(request.runner) if request.runner else parent.runner
+        model_spec = self._build_model(request.model) if request.model else parent.model
+        environment_spec = self._build_environment(request.environment) if request.environment else parent.environment
+        runner_spec = self._build_runner(request.runner) if request.runner else parent.runner
         
         updated_test = parent.update(
             name=name,
@@ -52,10 +52,43 @@ class TestService():
         )
         
         return self.repo.update(db, updated_test)
+    
+    
+    def _build_model(self, model_spec_input: ModelSpecInput) -> ModelSpec:
+        if model_spec_input.type == ModelType.PLATFORM:
+            return ModelSpec.create_platform(model_spec_input.model_id)
+        if model_spec_input == ModelType.EXTERNAL:
+            return ModelSpec.create_external(model_spec_input.endpoint, model_spec_input.key)
+        raise InvalidModelConfiguration(f"invalid model type: {model_spec_input.type}")
+    
         
+    def _build_environment(self, environment_spec_input: EnvironmentSpecInput) -> EnvironmentSpec:
+        if environment_spec_input.type == EnvType.MITIGATION:
+            self._build_prompt_environment(environment_spec_input.mitigations, environment_spec_input.system_prompt)
+        if environment_spec_input.type == EnvType.CUSTOM:
+            return EnvironmentSpec.create_from_system_prompt(environment_spec_input.system_prompt)
+        raise InvalidModelConfiguration(f"invalid environment type: {environment_spec_input.type}")
         
-    def get(self, id: str) -> Test:
-        test = self.repo.get_by_id(id)
+    
+    def _build_prompt_environment(self, mitigations: List[str], system_prompt: Optional[str] = None):
+        try:
+            compiled_prompt = PromptCompiler.compile(mitigations, system_prompt)
+        except UnknownMitigation as e:
+            raise InvalidModelConfiguration(f"invalid model configuration: {e}")
+        
+        return EnvironmentSpec.create_from_mitigations(mitigations, system_prompt)
+        
+    
+    def _build_runner(self, runner_spec_input: RunnerSpecInput) -> RunnerSpec:
+        if runner_spec_input.type == RunnerType.PROMPT:
+            return RunnerSpec.create_prompt(runner_spec_input.context)
+        if runner_spec_input.type == RunnerType.FRAMEWORK:
+            return RunnerSpec.create_framework()
+        raise InvalidModelConfiguration(f"invalid runner type: {runner_spec_input.type}")
+        
+    
+    def get(self, db: Session, id: str) -> Test:
+        test = self.repo.get_by_id(db, id)
         
         if not test:
             raise NotFoundError("test not found")
@@ -64,11 +97,11 @@ class TestService():
             
         
     
-    def list_all(self) -> List[Test]:
-        return self.repo.list_all()
+    def list_all(self, db: Session) -> List[Test]:
+        return self.repo.list_all(db)
             
             
-    async def run(self, db: Session, id: str, request: RunTestInput) -> TestResult:
+    async def run(self, db: Session, id: str, message: Message) -> TestResult:
         test = self.repo.get_by_id(db, id)
         
         if not test:
@@ -76,10 +109,10 @@ class TestService():
         
         runner = self._resolve_runner(test.runner.type)
         
-        result = await runner.run(test, Message("user", request.prompt) if request.prompt else None)
+        result = await runner.run(test, message if message else None)
         
         if test.runner.type == RunnerType.PROMPT:
-            self._update_context_with_response(db, id, test, request.prompt, result.output)
+            self._update_context_with_response(db, test, message, result.output)
         
         return result 
     
@@ -92,8 +125,9 @@ class TestService():
         raise InvalidModelConfiguration(f"unsupported runner type: {runner_type}")
     
     
-    def _update_context_with_response(self, db: Session, id: str, test: Test, prompt: str, response: str):
+    def _update_context_with_response(self, db: Session, test: Test, message: Message, response: str):
         test.runner.context.extend([
-            Message(role="user",content=prompt),
+            message,
             Message(role="system", content=response)
         ])
+        self.repo.update(db, test)
