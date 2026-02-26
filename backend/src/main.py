aad68e4402581aca989ee91d2a74be3f0bd8187a
teo-montero <teo.bonet.24@ucl.ac.uk>
2026-02-26 08:36:41 +0000

    task: added env loading and frontend allowance

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
 from infra.persistance.db import Base, engine
 from api.routes import test_router
 from container import Container
 
 Base.metadata.create_all(bind=engine)
 
+load_dotenv()
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 container = Container()
 
